2b13ed86e0d47525a27f707a0b39601453e3e89b
zengwei18488 <wei.zeng@advantech.com.cn>
2018-10-29 09:35:33 +0800

    add adv config for debian

---
+++ mk-adv.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+TARGET_ROOTFS_DIR="binary"
+
+echo "in mk-adv.sh"
+
+#---------------Overlay--------------
+echo "1.copy overlay"
+sudo cp -rf overlay-adv/* $TARGET_ROOTFS_DIR/
+
+echo "2.install/remove/adjust debian"
+
+cat <<EOF | sudo chroot $TARGET_ROOTFS_DIR
+
+#---------------Remove--------------
+apt-get --purge remove -y chromium
+apt-get --purge remove -y firefox
+
+#---------------Install--------------
+apt-get update
+apt-get install -y mtd-utils
+apt-get install -y i2c-tools
+#for rpmb
+apt-get install -y mmc-utils
+#for 4G
+apt-get install -y libpcap0.8:armhf ppp
+ap-get install -y usb-modeswitch mobile-broadband-provider-info modemmanager
+#for browser
+apt-get install -y chromium=63.0.3239.84-1~deb9u1
+apt install -t sid firefox
+
+#---------------Adjust--------------
+#for usb otg
+update-rc.d S50usbdevice defaults
+
+#for login
+sed  -i 's/--autologin root//'  /lib/systemd/system/serial-getty@.service
+echo "linaro:123456" | chpasswd
+echo "root:123456" | chpasswd
+
+#adduser
+#adduser --gecos adv --disabled-login adv
+#echo "adv:adv" | chpasswd
+
+
+#locale
+sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+#timezone
+timedatectl set-timezone Asia/Shanghai
+
+#---------------Clean--------------
+rm -rf /var/lib/apt/lists/*
+
+EOF
+
